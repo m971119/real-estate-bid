de1c1b292b526751464750d053565e49b18590a2
Michelle Lin <m971119@gmail.com>
2024-10-12 22:37:50 +1000

    feat: add monthly payment caculation

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
-    <Box class="md:col-span-7 flex items-center w-full">
-      <span class=" w-full text-center font-medium text-gray-500"> images</span>
+  <div class="flex flex-col-reverse gap-4 md:grid md:grid-cols-12">
+    <Box class="flex w-full items-center md:col-span-7">
+      <span class="w-full text-center font-medium text-gray-500"> images</span>
     </Box>
-    <div class="md:col-span-5 flex flex-col gap-4">
+    <div class="flex flex-col gap-4 md:col-span-5">
       <Box>
         <template #header>Info</template>
         <Price :price="listing.price" class="text-2xl font-bold" />
@@ -11,8 +11,31 @@
         <ListingAddress :listing="listing" class="text-gray-500" />
       </Box>
       <Box>
-        <template #header>Offer</template>
-        Make An Offer
+        <template #header>Monthly Payment</template>
+        <div>
+          <label class="label">Interest rate ({{ interestRate }}%)</label>
+          <input
+            v-model.number="interestRate"
+            type="range"
+            min="0.1"
+            max="30"
+            step="0.1"
+            class="h-4 w-full cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
+          />
+          <label class="label">Duration ({{ duration }} years)</label>
+          <input
+            v-model.number="duration"
+            type="range"
+            min="3"
+            max="35"
+            step="1"
+            class="h-4 w-full cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
+          />
+          <div class="mt-2 text-gray-600 dark:text-gray-300">
+            <div class="text-gray-400">Your monthly payment</div>
+            <Price :price="monthlyPayment" class="text-3xl" />
+          </div>
+        </div>
       </Box>
     </div>
   </div>
@@ -23,7 +46,22 @@ import ListingAddress from '@/Components/ListingAddress.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
-defineProps({
+import { ref, computed } from 'vue'
+
+const interestRate = ref(2.5)
+const duration = ref(25)
+const props = defineProps({
   listing: Object,
+})
+const monthlyPayment = computed(() => {
+  const principle = props.listing.price
+  const monthlyInterest = interestRate.value / 100 / 12
+  const numberOfPaymentMonths = duration.value * 12
+  return (
+    (principle *
+      monthlyInterest *
+      Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) /
+    (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
+  )
 })
 </script>
